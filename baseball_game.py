377e3f0c0774dd577b6d0c81b8e6b4f2f75a6000
JongHyeon Moon <dobest27@gmail.com>
2021-01-20 17:07:32 +0900

    fix: fix logical error on is_validated_number

--- baseball_game.py
+++ baseball_game.py
@@ -116,7 +116,7 @@ def is_validated_number(user_input_number: str) -> bool:
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
 
     result = is_digit(user_input_number) and is_between_100_and_999(
-        user_input_number) and is_duplicated_number(user_input_number)
+        user_input_number) and is_duplicated_number(user_input_number) is False
     # ==================================
     return result
 
